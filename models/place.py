e78b7e3afa07e19531dfc4e89fa2deb72cb4a592
Manalhub <manalrahali24@gmail.com>
2023-09-19 12:25:08 -0700

    Update Amenity and Place

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -21,10 +21,23 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        amenity_ids = []
+
+        # Create Many-to-Many relationship
+        # with Amenity using place_amenity table
+        place_amenity = Table(
+            'place_amenity', Base.metadata,
+            Column('place_id', String(60), ForeignKey('places.id'),
+                   primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                   primary_key=True, nullable=False)
+        )
+
+        # Define amenities relationship with viewonly=False
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False)
 
         reviews = relationship("Review", backref="place",
-                               cascade="all, delete")
+                               cascade="all, delete-orphan")
 
     else:
         city_id = ""
@@ -39,7 +52,6 @@ class Place(BaseModel, Base):
         longitude = 0.0
         amenity_ids = []
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def reviews(self):
             """ Getter attribute that returns a list of Review instances
@@ -47,3 +59,24 @@ class Place(BaseModel, Base):
             all_reviews = storage.all(Review)
             return [review for review in all_reviews.values()
                     if review.place_id == self.id]
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def amenities(self):
+            """ Getter attribute that returns the list of Amenity instances
+            based on the attribute amenity_ids that contains all Amenity.id
+            linked to the Place """
+            amenity_list = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.get(Amenity, amenity_id)
+                if amenity:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """ Setter attribute that handles append method for adding an
+            Amenity.id to the attribute amenity_ids. This method should
+            accept only Amenity object, otherwise, do nothing. """
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
